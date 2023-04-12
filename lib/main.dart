c4b9e2907f44e8513f77f2caa7235d453abf6e0c
Ibrahim Fathi <117414627+Ibrahimfathi96@users.noreply.github.com>
2023-04-12 03:44:38 +0200

    init the application

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(const ChatGptApplication());
+}
+
+class ChatGptApplication extends StatelessWidget {
+  const ChatGptApplication({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
