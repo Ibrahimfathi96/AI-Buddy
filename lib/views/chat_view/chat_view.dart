f1d42696dbe88b7a6821102324582990194f2b79
Ibrahim Fathi <117414627+Ibrahimfathi96@users.noreply.github.com>
2023-04-16 19:27:54 +0200

    add chat widger animation

--- lib/views/chat_view/chat_view.dart
+++ lib/views/chat_view/chat_view.dart
@@ -1,13 +1,14 @@
+import 'dart:developer';
 import 'package:chatgpt_application/constants/constants.dart';
-import 'package:chatgpt_application/models/chat_model.dart';
+import 'package:chatgpt_application/providers/chats_provider.dart';
 import 'package:chatgpt_application/providers/models_provider.dart';
-import 'package:chatgpt_application/services/api_services.dart';
 import 'package:chatgpt_application/services/assets_manager.dart';
 import 'package:chatgpt_application/services/services.dart';
 import 'package:chatgpt_application/views/widgets/chat_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:provider/provider.dart';
+import '../widgets/text_widget.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -18,59 +19,70 @@ class HomeView extends StatefulWidget {
 
 class _HomeViewState extends State<HomeView> {
   bool _isTyping = false;
-  late TextEditingController textEditingController;
 
+  late TextEditingController textEditingController;
+  late ScrollController _listScrollController;
+  late FocusNode focusNode;
   @override
   void initState() {
+    _listScrollController = ScrollController();
     textEditingController = TextEditingController();
+    focusNode = FocusNode();
     super.initState();
   }
 
   @override
   void dispose() {
+    _listScrollController.dispose();
     textEditingController.dispose();
+    focusNode.dispose();
     super.dispose();
   }
 
-  List<ChatModel> chatList = [];
+  // List<ChatModel> chatList = [];
   @override
   Widget build(BuildContext context) {
     final modelsProvider = Provider.of<ModelsProvider>(context);
+    final chatProvider = Provider.of<ChatProvider>(context);
     return Scaffold(
       appBar: AppBar(
-        actions: [
-          IconButton(
-              onPressed: () async {
-                await Services.showModalSheet(context: context);
-              },
-              icon: const Icon(
-                Icons.more_vert_outlined,
-                color: Colors.white,
-              ))
-        ],
         elevation: 2,
-        title: const Text('AI Buddy'),
         leading: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Image.asset(AssetsManager.openaiLogo),
         ),
+        title: const Text("ChatGPT"),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              await Services.showModalSheet(context: context);
+            },
+            icon: const Icon(Icons.more_vert_rounded, color: Colors.white),
+          ),
+        ],
       ),
       body: SafeArea(
         child: Column(
           children: [
-            Expanded(
+            Flexible(
               child: ListView.builder(
-                itemBuilder: (context, index) => ChatWidget(
-                  chatIndex: chatList[index].chatIndex,
-                  msg: chatList[index].msg,
-                ),
-                itemCount: chatList.length,
-              ),
+                  controller: _listScrollController,
+                  itemCount: chatProvider.getChatList.length, //chatList.length,
+                  itemBuilder: (context, index) {
+                    return ChatWidget(
+                      msg: chatProvider
+                          .getChatList[index].msg, // chatList[index].msg,
+                      chatIndex: chatProvider.getChatList[index]
+                          .chatIndex, //chatList[index].chatIndex,
+                      shouldAnimate:
+                          chatProvider.getChatList.length - 1 == index,
+                    );
+                  }),
             ),
             if (_isTyping) ...[
               const SpinKitThreeBounce(
-                size: 18,
                 color: Colors.white,
+                size: 18,
               ),
             ],
             const SizedBox(
@@ -79,31 +91,34 @@ class _HomeViewState extends State<HomeView> {
             Material(
               color: cardColor,
               child: Padding(
-                padding: const EdgeInsets.all(12.0),
+                padding: const EdgeInsets.all(8.0),
                 child: Row(
                   children: [
                     Expanded(
                       child: TextField(
+                        focusNode: focusNode,
                         style: const TextStyle(color: Colors.white),
                         controller: textEditingController,
                         onSubmitted: (value) async {
-                          await sendMessageFCT(modelsProvider: modelsProvider);
+                          await sendMessageFCT(
+                              modelsProvider: modelsProvider,
+                              chatProvider: chatProvider);
                         },
                         decoration: const InputDecoration.collapsed(
-                          hintText: 'How Can I Help You?',
-                          hintStyle: TextStyle(color: Colors.grey),
-                        ),
+                            hintText: "How can I help you",
+                            hintStyle: TextStyle(color: Colors.grey)),
                       ),
                     ),
                     IconButton(
-                      onPressed: () async {
-                        await sendMessageFCT(modelsProvider: modelsProvider);
-                      },
-                      icon: const Icon(
-                        Icons.send,
-                        color: Colors.white,
-                      ),
-                    ),
+                        onPressed: () async {
+                          await sendMessageFCT(
+                              modelsProvider: modelsProvider,
+                              chatProvider: chatProvider);
+                        },
+                        icon: const Icon(
+                          Icons.send,
+                          color: Colors.white,
+                        ))
                   ],
                 ),
               ),
@@ -114,19 +129,65 @@ class _HomeViewState extends State<HomeView> {
     );
   }
 
-  Future<void> sendMessageFCT({required ModelsProvider modelsProvider}) async {
+  void scrollListToEND() {
+    _listScrollController.animateTo(
+        _listScrollController.position.maxScrollExtent,
+        duration: const Duration(seconds: 2),
+        curve: Curves.easeOut);
+  }
+
+  Future<void> sendMessageFCT(
+      {required ModelsProvider modelsProvider,
+      required ChatProvider chatProvider}) async {
+    if (_isTyping) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: TextWidget(
+            label: "You cant send multiple messages at a time",
+          ),
+          backgroundColor: Colors.red,
+        ),
+      );
+      return;
+    }
+    if (textEditingController.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: TextWidget(
+            label: "Please type a message",
+          ),
+          backgroundColor: Colors.red,
+        ),
+      );
+      return;
+    }
     try {
+      String msg = textEditingController.text;
       setState(() {
         _isTyping = true;
+        // chatList.add(ChatModel(msg: textEditingController.text, chatIndex: 0));
+        chatProvider.addUserMessage(msg: msg);
+        textEditingController.clear();
+        focusNode.unfocus();
       });
-      chatList = await ApiService.sendMessage(
-          message: textEditingController.text,
-          modelId: modelsProvider.getCurrentModel);
+      await chatProvider.sendMessageAndGetAnswers(
+          msg: msg, chosenModelId: modelsProvider.getCurrentModel);
+      // chatList.addAll(await ApiService.sendMessage(
+      //   message: textEditingController.text,
+      //   modelId: modelsProvider.getCurrentModel,
+      // ));
       setState(() {});
-    } catch (ex) {
-      debugPrint("error $ex");
+    } catch (error) {
+      log("error $error");
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: TextWidget(
+          label: error.toString(),
+        ),
+        backgroundColor: Colors.red,
+      ));
     } finally {
       setState(() {
+        scrollListToEND();
         _isTyping = false;
       });
     }
--- lib/views/widgets/chat_widget.dart
+++ lib/views/widgets/chat_widget.dart
@@ -1,13 +1,19 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:chatgpt_application/constants/constants.dart';
 import 'package:chatgpt_application/services/assets_manager.dart';
-import 'package:chatgpt_application/views/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
+import 'text_widget.dart';
 
 class ChatWidget extends StatelessWidget {
+  const ChatWidget(
+      {super.key,
+      required this.msg,
+      required this.chatIndex,
+      this.shouldAnimate = false});
+
   final String msg;
   final int chatIndex;
-  const ChatWidget({super.key, required this.msg, required this.chatIndex});
-
+  final bool shouldAnimate;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -23,16 +29,41 @@ class ChatWidget extends StatelessWidget {
                   chatIndex == 0
                       ? AssetsManager.userImage
                       : AssetsManager.botImage,
-                  width: 30,
                   height: 30,
+                  width: 30,
                 ),
                 const SizedBox(
                   width: 8,
                 ),
                 Expanded(
-                  child: TextWidget(
-                    label: msg,
-                  ),
+                  child: chatIndex == 0
+                      ? TextWidget(
+                          label: msg,
+                        )
+                      : shouldAnimate
+                          ? DefaultTextStyle(
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 16),
+                              child: AnimatedTextKit(
+                                  isRepeatingAnimation: false,
+                                  repeatForever: false,
+                                  displayFullTextOnTap: true,
+                                  totalRepeatCount: 1,
+                                  animatedTexts: [
+                                    TyperAnimatedText(
+                                      msg.trim(),
+                                    ),
+                                  ]),
+                            )
+                          : Text(
+                              msg.trim(),
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 16),
+                            ),
                 ),
                 chatIndex == 0
                     ? const SizedBox.shrink()
@@ -50,7 +81,7 @@ class ChatWidget extends StatelessWidget {
                           Icon(
                             Icons.thumb_down_alt_outlined,
                             color: Colors.white,
-                          ),
+                          )
                         ],
                       ),
               ],
